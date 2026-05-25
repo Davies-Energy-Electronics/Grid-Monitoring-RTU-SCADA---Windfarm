afbee0d06cc0636aacd2099251fe7c54ce3e77f3
Jack Davies <davies.professional1@gmail.com>
2026-05-25 15:00:09 +0100

    Create config.h

---
+++ firmware/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define ADC_PIN             34
+#define ADC_RESOLUTION      4095.0
+#define ADC_REFERENCE       3.3
+
+#define SAMPLE_RATE         2000
+#define FUNDAMENTAL_FREQ    50
+
+#define SAMPLES_PER_CYCLE   40
+
+#define ADC_BIAS            1.65
+
+#endif
